60bfa1e2ad654140c06e4d3ac8c1e26ba8566047
Sam <samuel.jones@q-free.com>
2016-03-08 09:59:12 -0500

    Render cells correctly.

--- ng2-material/components/data_table/data_table_row.ts
+++ ng2-material/components/data_table/data_table_row.ts
@@ -8,6 +8,7 @@ import {
   Directive,
   DynamicComponentLoader,
   ElementRef,
+  EmbeddedViewRef,
   Host,
   Input,
   Pipe, PipeTransform,
@@ -16,6 +17,7 @@ import {
   Renderer,
   TemplateRef,
   View,
+  ViewRef,
   ViewContainerRef,
   ViewEncapsulation,
 } from "angular2/core";
@@ -23,76 +25,43 @@ import {MdCheckbox} from "../checkbox/checkbox";
 // import {MdDataCell} from  './data_table_cell';
 
 @Directive({
-  selector: '[md-data-cell]',
-  /*template: `
-    
-  `*/
+  selector: '[md-data-cell]'
 })
 export class MdDataCell {
   @Input() data: any;
   @Input() templ: TemplateRef;
 
-  constructor(private _viewContainer: ViewContainerRef) {}
+  childView: EmbeddedViewRef;
+
+  constructor(private _viewContainer: ViewContainerRef,
+    private _elementRef: ElementRef) { }
 
   ngAfterContentInit() {
-    this._viewContainer.createEmbeddedView(this.templ, 0);
+    console.log('cell.data', this.data);
+    this.childView = this._viewContainer.createEmbeddedView(this.templ);
+    // console.log('this.childView', this.childView);
+    this.childView.setLocal('data', this.data);
   }
 }
 
 @Component({
   selector: '[md-data-row]',
   directives: [MdCheckbox, MdDataCell],
-  inputs: ['data'],
   template: `
     <td *ngIf="selectable" class="md-data-check-cell">
       <md-checkbox (click)="rowCheckClick"></md-checkbox>
     </td>
-<!--     <template ngFor #cell [ngForOf]="cells" [ngForTemplate]="cell">
-    <td><cell></cell></td>
-  </template>
-   -->
-   <template ngFor #cell [ngForOf]="templ" [ngForTemplate]="cell">
-     <td md-data-cell [data]="data" [templ]="cell"></td>
-   </template>
+    <td *ngFor="#cell of templ">
+      <template md-data-cell [data]="data" [templ]="cell"></template>
+    </td>
   `
 })
 export class MdDataRow {
   @Input() data: any;
   @Input() selectable: boolean;
-  @Input() cells: QueryList<MdDataCell>;
   @Input() templ: QueryList<TemplateRef>;
 
   constructor(private _viewContainer: ViewContainerRef,
       private _elementRef: ElementRef) {
-    // console.log('hello data row component.');
   }
-
-  ngAfterContentInit() {
-    console.log("row data", this.data);
-    console.log("view container", this._viewContainer);
-    console.log("elementRef", this._elementRef);
-    // console.log("cells", this.cells);
-    console.log("templ", this.templ);
-    // this._viewContainer.insert(this.cells);
-    // this.cells.map(cell => {
-    //   this._viewContainer.insert(cell);
-    // });
-    // this._viewContainer.createEmbeddedView(this.templ, 0);
-    // this._elementRef.element.nativeElement.appendChild
-  }
-
-  //from ngFor:
-  /*private _bulkInsert(tuples: RecordViewTuple[]): RecordViewTuple[] {
-    tuples.sort((a, b) => a.record.currentIndex - b.record.currentIndex);
-    for (var i = 0; i < tuples.length; i++) {
-      var tuple = tuples[i];
-      if (isPresent(tuple.view)) {
-        this._viewContainer.insert(tuple.view, tuple.record.currentIndex);
-      } else {
-        tuple.view =
-          this._viewContainer.createEmbeddedView(this._templateRef, tuple.record.currentIndex);
-      }
-    }
-    return tuples;
-  }*/
 }
